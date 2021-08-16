6e2e44a7f4d5df1b2293994a0f2ecd22bb66f084
OArtyomov <Oleg_Artyomov@ukr.net>
2021-08-16 07:29:41 -0400

    Code cleanup

--- src/test/java/com/hcl/hclmessaging/test/AbstractBaseTest.java
+++ src/test/java/com/hcl/hclmessaging/test/AbstractBaseTest.java
@@ -1,7 +1,6 @@
 package com.hcl.hclmessaging.test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.tomakehurst.wiremock.WireMockServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,11 +18,6 @@ public abstract class AbstractBaseTest {
     @Autowired
     protected ObjectMapper objectMapper;
 
-    @Autowired
-    protected WireMockServer wireMockServer;
-
     public static final String WIRE_MOCK_PORT_PROPERTY_NAME = "wiremock.port";
-
-    public static final String MAPPING_PATH_PROPERTY_NAME = "mapping.path";
 
 }
--- src/test/java/com/hcl/test/wiremock/MockServerConfiguration.java
+++ src/test/java/com/hcl/test/wiremock/MockServerConfiguration.java
@@ -1,11 +1,7 @@
 package com.hcl.test.wiremock;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.common.ClasspathFileSource;
-import com.github.tomakehurst.wiremock.common.FileSource;
-import com.github.tomakehurst.wiremock.common.SingleRootFileSource;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import com.github.tomakehurst.wiremock.standalone.JsonFileMappingsSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.MapPropertySource;
@@ -15,9 +11,7 @@ import java.util.Map;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static com.google.common.collect.ImmutableMap.of;
-import static com.hcl.hclmessaging.test.AbstractBaseTest.MAPPING_PATH_PROPERTY_NAME;
 import static com.hcl.hclmessaging.test.AbstractBaseTest.WIRE_MOCK_PORT_PROPERTY_NAME;
-import static org.springframework.util.ResourceUtils.CLASSPATH_URL_PREFIX;
 import static org.springframework.util.SocketUtils.findAvailableTcpPort;
 
 @Configuration
@@ -29,20 +23,10 @@ public class MockServerConfiguration {
         Map<String, Object> source = of(WIRE_MOCK_PORT_PROPERTY_NAME, availableTcpPort);
         environment.getPropertySources().addLast(new MapPropertySource("wireMock", source));
 
-        WireMockConfiguration wireMockConfiguration =
-                wireMockConfig()
-                        .port(availableTcpPort)
-                        .mappingSource(new JsonFileMappingsSource(buildFileSource(environment)));
+        WireMockConfiguration wireMockConfiguration = wireMockConfig().port(availableTcpPort);
         WireMockServer wireMockServer = new WireMockServer(wireMockConfiguration);
         wireMockServer.start();
         return wireMockServer;
     }
 
-    private FileSource buildFileSource(StandardEnvironment environment) {
-        String value = environment.getRequiredProperty(MAPPING_PATH_PROPERTY_NAME, String.class);
-        if (value.startsWith(CLASSPATH_URL_PREFIX)) {
-            return new ClasspathFileSource(value.replaceAll(CLASSPATH_URL_PREFIX, ""));
-        }
-        return new SingleRootFileSource(value);
-    }
 }
